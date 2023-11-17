0684c4f8e08adcc17623b351762297a406f06a51
Danin Sadžak <daninsadzak@gmail.com>
2023-11-17 17:51:12 +0100

    Added the main event loop

--- script.c
+++ script.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main() {
-    
+    int actionPerformed = 0;
+    while (1) {
+        if ((GetAsyncKeyState(VK_SPACE) & 0x8001) && (!actionPerformed)) {
+            printf("Hello, World!\n");
+            actionPerformed = 1;
+        } else if (!(GetAsyncKeyState(VK_SPACE)) & 0x8001) {
+            actionPerformed = 0;
+        }
+    }
     return 0;
 }
